99ee5c2692e2eda32ffd2c1c90b84f5a64b9058e
excelsisv <valenciaexcelsis@gmail.com>
2026-07-07 12:49:38 +0800

    Add files via upload

---
+++ problem5.4.py
@@ -0,0 +1,12 @@
+#Name: Excelsis Valencia
+#School: FEU-TECH
+#Machine Problem - 5.4
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+
+output = []
+for i in range(len(letters)):
+    output.append(letters[i] * (i + 1))
+
+
+print("OUTPUT IS", output)
